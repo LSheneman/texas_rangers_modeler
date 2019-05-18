97d0ddad9dea28615541e590e9985a4697e389e4
Leigh Sheneman <leighsheneman@gmail.com>
2019-05-18 14:24:17 -0400

    project creation and source code to acquire tweets

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+TWITTER_CONFIG = {
+    'consumer_key' : 'kaq8diqqbIyzcpOKGybusXdlM',
+    'consumer_secret' : 'JnhNLlDOa3hfUipkvaWnr8ix6x7QhZXE3K8FkBrkAy7qwYxauL',
+    'access_token' : '480588144-2Ikyo1r909rFj3vJETk119KYJ4f1ph9Aa6x5EAa4',
+    'access_token_secret' : 'pXumU4xH2gxo0kzm0k0fDiT8pa59YgEBZaGpYQhgU98lB'
+}
